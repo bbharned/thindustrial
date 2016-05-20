5c1d8c9fbe2291e92e04cc25365853611cfdbcc8
Bryan Harned <bharned@thinmanager.com>
2016-05-20 13:15:54 -0400

    small style changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
