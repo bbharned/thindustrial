eae93f4087213e7388b49be77492f3663e1d4e55
Bryan Harned <bharned@thinmanager.com>
2016-06-20 09:21:38 -0400

    added login when registering and more permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 before_action :set_user, only: [:edit, :update, :show]
-before_action :require_user
+before_action :require_user, only: [:index, :edit, :update, :show]
 before_action :require_same_user, only: [:edit, :update, :destroy]
 
 def index 
@@ -16,8 +16,9 @@ def create
 	@user = User.new(user_params)
 	
 	if @user.save
+      session[:user_id] = @user.id
   		flash[:success] = "Welcome to Thindustrial #{@user.firstname}"
-  		redirect_to courses_path
+  		redirect_to user_path(@user)
 	else
   		render 'new'
 	end
