78b950e25634e9ffff5bf02871bda6b31e482f64
Bryan Harned <bharned@thinmanager.com>
2016-07-01 11:00:06 -0400

    added scheduling functionality

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
-	before_action :require_user, only: [:index, :edit, :update, :show]
-	
+	#before_action :require_user, only: [:index, :create, :edit, :update, :show]
+	#@courses = Course.all
+
 	def index
 		if !current_user.admin
 	      flash[:danger] = "Only admins can view others schedules"
@@ -12,10 +13,28 @@ class SchedulesController < ApplicationController
 	end
 
 	def new
+		@schedule = Schedule.new
+	end
 
+	def create	
+	  @course = Course.find(params[:course_id])
+	  @schedule = Schedule.new(user_id: current_user.id, course_id: @course.id, timeblock: @course.timeblock)
+
+        if @schedule.save
+          flash[:success] = "The selected course has been added to your schedule"
+  		  redirect_to user_path(current_user)
+        else
+          flash[:danger] = "There was a problem adding this class"
+  		  redirect_to user_path(current_user)
+        end
 	end
 
 	def show
-		#@schedule = Schedule.find(params[:id])
+		
 	end
+
+	private
+		
+		# Never trust parameters from the scary internet, only allow the white list through.
+	    
 end
