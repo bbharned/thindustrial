dc850a06fc8a30db14291fb66042ff9cd682e1bc
Bryan Harned <bharned@thinmanager.com>
2016-07-08 13:57:24 -0400

    added validation for schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,6 @@ class Schedule < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :course
 	validates :timeblock, presence: true 
+	validates :user_id, presence: true 
+	validates :course_id, presence: true 
 end
