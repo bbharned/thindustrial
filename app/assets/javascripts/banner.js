a0a4f554992a8c0f43d025d233f7b9689ffffff0
Bryan Harned <bbharned@gmail.com>
2016-08-03 16:46:34 -0400

    added lots of homepage styling and some javascript

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,25 @@
+$(document).on('ready', function() {
+    
+    $(".banner-dont-miss").animate({
+        height: '50'
+        }, 150, "linear", function() {
+            
+        }
+    });
+
+});
+
+/*
+(function(){
+    
+    "use strict";
+
+    $(".banner-dont-miss").animate({
+        height: '50'
+        }, 150, "linear", function() {
+
+        }
+    });
+
+})();
+*/
